c8a36192bc347994f9c6411ddd44fa9f7d1dcc48
Diego Aponte <diegaponte@gmail.com>
2024-11-10 21:40:25 -0500

    funcion leer datos

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+function readData() {
+    try {                                         
+        const data = fs.readFileSync(dbPath, 'utf-8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error("Error al leer la base de datos:", error);
+        return { clientes: [], productos: [], carrito: [] };
+    }
+}
