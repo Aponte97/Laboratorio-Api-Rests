50cb6eaa42e19841401b5b37e77ae50453469deb
Diego Aponte <diegaponte@gmail.com>
2024-11-10 21:50:58 -0500

    endpoint para mostrar por id productos, clientes y carrito de compras

--- index.js
+++ index.js
@@ -47,3 +47,37 @@ app.get('/productos', (req, res) => {
     const data = readData();
     res.json(data.productos);
 });
+app.get('/clientes/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const data = readData();
+    const cliente = data.clientes.find(c => c.id_cliente === id);
+    if (cliente) {
+        res.json(cliente);
+    } else {
+        res.status(404).json("Cliente no encontrado");
+    }
+});
+
+// Endpoint para mostrar un producto por id
+app.get('/productos/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const data = readData();
+    const producto = data.productos.find(p => p.id === id);
+    if (producto) {
+        res.json(producto);
+    } else {
+        res.status(404).json("Producto no encontrado");
+    }
+});
+
+// Endpoint para mostrar el carrito de un cliente por id
+app.get('/carrito/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const data = readData();
+    const carrito = data.carrito.find(c => c.id_cliente === id);
+    if (carrito) {
+        res.json(carrito);
+    } else {
+        res.status(404).json("Carrito no encontrado para este cliente");
+    }
+});
